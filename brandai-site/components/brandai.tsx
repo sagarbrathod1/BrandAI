0f7643fa16888271aee3d930b70b04d7fe7cc4d3
Sagar Rathod <97072541+sagarbrathod1@users.noreply.github.com>
2023-10-14 18:34:11 -0400

    app no longer functional message

--- brandai-site/components/brandai.tsx
+++ brandai-site/components/brandai.tsx
@@ -5,64 +5,77 @@ import Image from "next/image";
 import logo from "../public/openai.svg";
 
 const BrandAI: React.FC = () => {
+  const chLimit: number = 32;
 
-    const chLimit: number = 32;
+  const ENDPOINT: string =
+    "https://ok4bu1kov0.execute-api.us-east-2.amazonaws.com/prod/generate_snippet_and_keywords";
 
-    const ENDPOINT: string = "https://ok4bu1kov0.execute-api.us-east-2.amazonaws.com/prod/generate_snippet_and_keywords";
-    
-    const [prompt, setPrompt] = React.useState("");
-    const [snippet, setSnippet] = React.useState("");
-    const [keywords, setKeywords] = React.useState([]);
-    const [hasResult, setHasResult] = React.useState(false);
-    const [isLoading, setIsLoading] = React.useState(false);
+  const [prompt, setPrompt] = React.useState("");
+  const [snippet, setSnippet] = React.useState("");
+  const [keywords, setKeywords] = React.useState([]);
+  const [hasResult, setHasResult] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
 
-    const onSubmit = () => {
-        console.log("Submitting: " + prompt);
-        setIsLoading(true);
-        fetch(`${ENDPOINT}?prompt=${prompt}`)
-            .then((res) => res.json())
-            .then(onResult)
-    };
+  const onSubmit = () => {
+    console.log("Submitting: " + prompt);
+    setIsLoading(true);
+    fetch(`${ENDPOINT}?prompt=${prompt}`)
+      .then((res) => res.json())
+      .then(onResult);
+  };
 
-    const onResult = (data: any) => {
-        setSnippet(data.snippet);
-        setKeywords(data.keywords);
-        setHasResult(true);
-        setIsLoading(false);
-    };
+  const onResult = (data: any) => {
+    setSnippet(data.snippet);
+    setKeywords(data.keywords);
+    setHasResult(true);
+    setIsLoading(false);
+  };
 
-    const onReset = () => {
-        setPrompt("");
-        setHasResult(false);
-        setIsLoading(false);
-    };
+  const onReset = () => {
+    setPrompt("");
+    setHasResult(false);
+    setIsLoading(false);
+  };
 
-    let displayedElement = null;
+  let displayedElement = null;
 
-    if (hasResult) {
-        displayedElement = <Results snippet={snippet} keywords={keywords} onBack={onReset} prompt={prompt} />
-    } else {
-        displayedElement = <Form prompt={prompt} setPrompt={setPrompt} onSubmit={onSubmit} isLoading={isLoading} chLimit={chLimit} />;
-    }
-
-    const gradientTextStyle = "text-white text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 font-medium w-fit mx-auto";
-
-
-    return (
-        <div className="h-screen flex">
-            <div className="max-w-md m-auto p-2">
-                <div className="bg-slate-800 p-6 rounded-md text-white">
-                    <div className="text-center my-6">
-                        <Image src={logo} width={65} height={65} />
-                        <h1 className={gradientTextStyle + " text-4xl"}>
-                            Brand AI
-                        </h1>
-                    </div>
-                    {displayedElement}
-                </div>
-            </div>
-        </div>
+  if (hasResult) {
+    displayedElement = (
+      <Results
+        snippet={snippet}
+        keywords={keywords}
+        onBack={onReset}
+        prompt={prompt}
+      />
     );
-}
+  } else {
+    displayedElement = (
+      <Form
+        prompt={prompt}
+        setPrompt={setPrompt}
+        onSubmit={onSubmit}
+        isLoading={isLoading}
+        chLimit={chLimit}
+      />
+    );
+  }
+
+  const gradientTextStyle =
+    "text-white text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-blue-500 font-medium w-fit mx-auto";
+
+  return (
+    <div className="h-screen flex">
+      <div className="max-w-md m-auto p-2">
+        <div className="bg-slate-800 p-6 rounded-md text-white">
+          <div className="text-center my-6">
+            <Image src={logo} width={65} height={65} />
+            <h1 className={gradientTextStyle + " text-4xl"}>Brand AI</h1>
+          </div>
+          {displayedElement}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default BrandAI;
--- brandai-site/components/form.tsx
+++ brandai-site/components/form.tsx
@@ -1,56 +1,75 @@
 interface FormProps {
-    prompt: string;
-    setPrompt: any;
-    onSubmit: any;
-    isLoading: boolean;
-    chLimit: number;
+  prompt: string;
+  setPrompt: any;
+  onSubmit: any;
+  isLoading: boolean;
+  chLimit: number;
 }
 
 const Form: React.FC<FormProps> = (props) => {
-
-    const isPromptValid = props.prompt.length < props.chLimit;
-    const updatePromptValue = (text: string) => {
-        if (text.length <= props.chLimit) {
-          props.setPrompt(text);
-        }
-      };
-    
-    let statusColor = "text-slate-500";
-    let statusText = null;
-    if (!isPromptValid) {
-        statusColor = "text-red-400";
-        statusText = `Input must be less than ${props.chLimit} characters.`;
+  const isPromptValid = props.prompt.length < props.chLimit;
+  const updatePromptValue = (text: string) => {
+    if (text.length <= props.chLimit) {
+      props.setPrompt(text);
     }
+  };
 
-    return (
-        <>
-            <div className="mb-6 text-slate-400 text-center">
-                <p>
-                    Please input your "product/brand" and I will generate an upbeat tagline and SEO keywords for you.
-                </p>
-            </div>
-            <input 
-                className="p-2 w-full rounded-md focus:outline-teal-400 focus:outline text-slate-700"
-                type="text" 
-                placeholder="i.e. yoga mat" 
-                value={props.prompt}
-                onChange={(e) => updatePromptValue(e.currentTarget.value)}
-            ></input>
-            <div className={statusColor + " flex justify-between my-2 mb-6 text-sm"}>
-                <div>
-                    {statusText}
-                </div>
-                <div>
-                    {props.prompt.length}/{props.chLimit}
-                </div>
-            </div>
-            <button 
-                className="bg-gradient-to-r from-emerald-400 to-blue-500 disabled:opacity-50 w-full p-2 rounded-md text-lg"
-                onClick={props.onSubmit} 
-                disabled={props.isLoading || !isPromptValid}>Submit</button>
-            <footer className="mt-6 font-thin text-xs text-slate-400 text-center">Powered by OpenAI</footer>
-        </>
-    );
+  let statusColor = "text-slate-500";
+  let statusText = null;
+  if (!isPromptValid) {
+    statusColor = "text-red-400";
+    statusText = `Input must be less than ${props.chLimit} characters.`;
+  }
+
+  return (
+    <>
+      <div className="mb-6 text-slate-400 text-center">
+        <p>
+          Please input your "product/brand" and I will generate an upbeat
+          tagline and SEO keywords for you.
+        </p>
+      </div>
+      <input
+        className="p-2 w-full rounded-md focus:outline-teal-400 focus:outline text-slate-700"
+        type="text"
+        placeholder="i.e. yoga mat"
+        value={props.prompt}
+        onChange={(e) => updatePromptValue(e.currentTarget.value)}
+      ></input>
+      <div className={statusColor + " flex justify-between my-2 mb-6 text-sm"}>
+        <div>{statusText}</div>
+        <div>
+          {props.prompt.length}/{props.chLimit}
+        </div>
+      </div>
+      <button
+        className="bg-gradient-to-r from-emerald-400 to-blue-500 disabled:opacity-50 w-full p-2 rounded-md text-lg"
+        onClick={props.onSubmit}
+        disabled={props.isLoading || !isPromptValid}
+      >
+        Submit
+      </button>
+      <div className="text-red-500 my-4 text-center">
+        Apologies, but this app is no longer functional due to my AWS Free Tier
+        access expiring. 😅
+      </div>
+      <div className="text-green-500 my-4 text-center">
+        Check out the source code{" "}
+        <a
+          href="https://github.com/sagarbrathod1/BrandAI"
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          here
+        </a>
+        .
+      </div>
+      <footer className="mt-6 font-thin text-xs text-slate-400 text-center">
+        Powered by OpenAI
+      </footer>
+    </>
+  );
 };
 
 export default Form;
